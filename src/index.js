ff9599302e653b60c7b7c28ac4770ed1cb612f8a
NONONOexe <g.kei0429@gmail.com>
2021-02-23 17:49:47 +0900

    🔧デザインの修正
    大幅にデザインを見直しました。

--- src/index.js
+++ src/index.js
@@ -1,169 +1,135 @@
-(function(){
-	var cont = "";
-	var midNumKey = 0;
-	var mid1NumFlag = false;
-	var mid2NumFlag = false;
-	var eqlFlag = false;
-	var flag = 0;
-	var midNum = [];
-	var ope;
-	var eqlNum;
-	var elems;
-	var ari;
+(function() {
+	var operand1 = "";
+	var operand2 = "";
+	var operator = "";
+	var result = "";
+	var inputedOperator = false;
+	var inputedOperand1 = false;
 
-	window.onload = function init(){
-		midNum[0] = document.getElementById("mid1num");
-		midNum[1] = document.getElementById("mid2num");
-		ope = document.getElementById("ope");
-		eqlNum = document.getElementById("eqlnum");
-		elems = document.getElementsByClassName("button");
-		ari = document.getElementsByClassName("symbol");
+	window.onload = function init() {
+		operand = document.getElementById("display-operand");
+		state = document.getElementById("display-state");
 
-		// 数字キーのイベントハンドラ
-		for(var i = 0;i < elems.length;i++){
-			elems[i].addEventListener("click",function(e){
-				pushNumber(e.target.dataset.number);
-			},false);
+		// イベントハンドラの設定
+		// 数字キー
+		numberButtons = document.getElementsByClassName("number");
+		for (numberButton of numberButtons) {
+			numberButton.addEventListener("click", (e) => pushNumber(e.target.dataset.number));
 		}
 
-		// 四則演算キーのイベントハンドラ
-		for(var i = 0;i < ari.length;i++){
-			ari[i].addEventListener("click",function(e){
-				pushSymbol(e.target.dataset.number);
-			},false);
+		// 四則演算キー
+		symbolButtons = document.getElementsByClassName("symbol");
+		for (symbolButton of symbolButtons) {
+			symbolButton.addEventListener("click", (e) => pushSymbol(e.target.dataset.symbol));
 		}
 
-		// ＝のイベントハンドラ
-		document.getElementById("eqlbtn").addEventListener("click",equal,false);
+		// Cキー
+		clrButton = document.getElementById("clr");
+		clrButton.addEventListener("click", (e) => clear());
+		
+		// ACキー
+		acButton = document.getElementById("ac");
+		acButton.addEventListener("click", (e) => allClear());
 
-		// ac のイベントハンドラ
-		document.getElementById("clr").addEventListener("click",allClear,false);
-	}
+		// イコールキー
+		eqlButton = document.getElementById("eql");
+		eqlButton.addEventListener("click", (e) => calc());
 
-	// 数字キーが押された際の処理
-	function pushNumber(new_num){
-
-		if( eqlFlag || eqlNum.value == "ERROR" || midNum[0].value == "NaN" ){
-			allClear();
+		function pushNumber(number) {
+			if (operand1.length != 0 && inputedOperator) {
+				operand.innerText = "";
+				inputedOperator = false;
+				inputedOperand1 = true;
+			}
+			if (inputedOperand1) {
+				if (14 <= operand2.length) {
+					state.innerText = "ERROR";
+					return;
+				}
+				operand2 += number;
+				operand.innerText = operand2;
+			} else {
+				if (14 <= operand1.length) {
+					state.innerText = "ERROR";
+					return;
+				}
+				operand1 += number;
+				operand.innerText = operand1;
+			}
 		}
 
-		if(new_num != "." && midNum[midNumKey].value == "0"){
-			midNum[midNumKey].value = new_num;
-		}else if(midNum[midNumKey] == ""){
-			midNum[midNumKey].value = "0.";
-		}else{
-			midNum[midNumKey].value += new_num;
+		function pushSymbol(symbol) {
+			if (result.length != 0) {
+				operand1 = result;
+				result = "";
+			}
+			switch (symbol) {
+				case "add":
+					state.innerText = "＋";
+					break;
+				case "sub":
+					state.innerText = "−";
+					break;
+				case "mul":
+					state.innerText = "×";
+					break;
+				case "div":
+					state.innerText = "÷";
+					break;
+				default:
+					state.innerText = "開発中";
+			}
+			operator = symbol;
+			inputedOperator = true;
 		}
 
-		mid1NumFlag = true;
-	}
-
-	// 演算子が押された際の処理
-	function pushSymbol(new_cont){
-
-		if(midNumKey == 0){
-			midNumKey = 1;
-		}else if( midNum[0].value == "NaN" || eqlNum.value == "ERROR"){
-			allClear();
-		}else if( mid1NumFlag || eqlFlag ){
-			equal();
-			console.log(parseFloat(eqlNum.value));
-			var num = parseFloat(eqlNum.value);
-			allClear();
-			pushNumber(num);
-			midNumKey = 1;
+		function clear() {
+			if (inputedOperand1) {
+				operand2 = "";
+			} else {
+				operand1 = "";
+			}
+			operand.innerText = "";
 		}
 
-		cont = new_cont;
-
-		if(midNum[1].value == ""){
-			midNum[1].value = "0";
+		function allClear() {
+			operand1 = "";
+			operand2 = "";
+			operator = "";
+			result = "";
+			inputedOperator = false;
+			inputedOperand1 = false;
+			operand.innerText = "";
+			state.innerText = "";
 		}
 
-		switch(cont){
-			case "add":
-				ope.textContent = ("+");
-				break;
-			case "sub":
-				ope.textContent = ("-");
-				break;
-			case "mul":
-				ope.textContent = ("×");
-				break;
-			case "div":
-				ope.textContent = ("÷");
-				break;
-		}
-
-		mid1NumFlag = false;
-	}
-
-	//＝を押したときの処理
-	function equal() {
-
-		//四則演算ごとの関数を呼び出す
-		switch(cont){
-			case "add":
-				// add関数で足し算の処理
-				add();
-				break;
-			case "sub":
-				// sub関数で引き算の処理
-				sub();
-				break;
-			case "mul":
-				// mul関数で掛け算の処理
-				mul();
-				break;
-			case "div":
-				// div関数で割り算の処理
-				div();
-				break;
-		}
-
-		eqlFlag = true;
-
-	}
-
-  // 足し算用の関数 add
-  function add(){
-		eqlNum.value = parseFloat(midNum[0].value) + parseFloat(midNum[1].value);
-	}
-  
-  // 引き算用の関数 sub
-	function sub(){
-		eqlNum.value = parseFloat(midNum[0].value) - parseFloat(midNum[1].value);
-	}
-
-	// 掛け算用の関数 mul
-	function mul() {
-		eqlNum.value = parseFloat(midNum[0].value) * parseFloat(midNum[1].value);
-	}
-
-	// 割り算の関数 div
-	function div() {
-		if(parseFloat(midNum[1].value) == 0){
-			eqlNum.value = "ERROR";
-		}else{
-			eqlNum.value = parseFloat(midNum[0].value) / parseFloat(midNum[1].value);
+		function calc() {
+			if (operator.length == 0) return;
+			if (operand2.length == 0) {
+				operator = "";
+				inputedOperator = false;
+				state.innerText = "";
+				return;
+			}
+			switch (operator) {
+				case "add":
+					result = parseFloat(operand1) + parseFloat(operand2);
+					break;
+				case "sub":
+					result = parseFloat(operand1) - parseFloat(operand2);
+					break;
+				case "mul":
+					result = parseFloat(operand1) * parseFloat(operand2);
+					break;
+				case "div":
+					result = parseFloat(operand1) / parseFloat(operand2);
+					break;
+			}
+			operand1 = "";
+			operand2 = "";
+			operator = "";
+			operand.innerText = result;
+			state.innerText = "";
 		}
 	}
-
-	/* 演習1 ここまで */
-
-	/* TODO: 演習2 以降は下記に自由に記入 */
-
-	/* 演習の編集範囲 おわり */
-
-	// ac を押したときの処理
-	function allClear(){
-		midNum[0].value = "0";
-		ope.textContent = " ";
-		midNum[1].value = "";
-		eqlNum.value = "";
-		mid1NumFlag = false;
-		eqlFlag = false;
-		midNumKey = 0;
-	}
-
 }());
